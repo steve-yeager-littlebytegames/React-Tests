dee43869ef1964fceaa3c61fefe36856ba73ae05
Steve Yeager <steve.yeager@littlebytegames.com>
2018-03-14 11:07:21 -0700

    Can submit match form

--- proto-react/client/components/MatchComp.jsx
+++ proto-react/client/components/MatchComp.jsx
@@ -3,47 +3,55 @@ import Match from '../src/match.js'
 
 export default class MatchComp extends React.Component {
     render() {
-        const id = this.props.match.id;
+        const index = this.props.match.id;
+        const namePrefix = "matches[" + index + "].";
+        const matchCharPrefix = namePrefix + "matchCharacters[";
 
         return (
             <div>
                 <div>
-                    <span>P1 Score</span>
-                    <input type="number" />
+                    <label>
+                        P1 Score
+                    <input type="number" name={namePrefix + "player1score"} />
+                    </label>
                 </div>
                 <div>
+                    <input type="hidden" name={matchCharPrefix + "0].leagueUserID"} value="26" />
                     <span>P1 Character</span>
-                    <select defaultValue="1" onChange={event => console.log(event.target.value)}>
-                        <option value="0">Mario</option>
-                        <option value="1">Link</option>
-                        <option>Sonic</option>
+                    <select name={matchCharPrefix + "0].characterID"}>
+                        <option value="72">Mario</option>
+                        <option value="73">Link</option>
+                        <option value="93">Sonic</option>
                     </select>
                 </div>
 
                 <div>
                     <span>P2 Score</span>
-                    <input type="number" />
+                    <input type="number" name={namePrefix + "player2score"} />
                 </div>
                 <div>
+                    <input type="hidden" name={matchCharPrefix + "1].leagueUserID"} value="1" />
                     <span>P2 Character</span>
-                    <select>
-                        <option>Mario</option>
-                        <option>Link</option>
-                        <option>Sonic</option>
+                    <select name={matchCharPrefix + "1].characterID"}>
+                        <option value="72">Mario</option>
+                        <option value="73">Link</option>
+                        <option value="93">Sonic</option>
                     </select>
                 </div>
 
                 <div>
-                    <span>Stage</span>
-                    <select>
-                        <option>Final Destination</option>
-                        <option>Battlefield</option>
-                        <option>Smashville</option>
-                    </select>
+                    <label>
+                        Stage
+                        <select name={namePrefix + "stageID"} >
+                            <option value="1">Final Destination</option>
+                            <option value="6">Battlefield</option>
+                            <option value="2">Smashville</option>
+                        </select>
+                    </label>
                 </div>
 
-                <button onClick={() => this.props.deleteMatch(id)}>Delete Match</button>
-            </div>
+                <button onClick={() => this.props.deleteMatch(index)}>Delete Match</button>
+            </div >
         );
     }
 }
--- proto-react/client/components/MatchForm.jsx
+++ proto-react/client/components/MatchForm.jsx
@@ -7,6 +7,23 @@ export default class MatchForm extends React.Component {
             <MatchComp key={m.id} match={m} deleteMatch={this.props.deleteMatch} />
         );
 
-        return <ol>{matchComps}</ol>
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="hidden" name="id" value="38" />
+                {matchComps}
+                <button disabled={this.props.matches.length === 0}>Submit</button>
+            </form>
+        );
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const data = new FormData(event.target);
+        fetch("https://localhost:44304/Sets/Submit", {
+            method: "POST",
+            body: data,
+            mode: "no-cors"
+        });
     }
 }
