c6dd94a65fec6f2d01ad36317fa2e8c3c19df212
Unknown <steve.yeager@littlebytegames.com>
2018-03-13 17:27:05 -0700

    Start match inputs

--- proto-react/client/components/MatchComp.jsx
+++ proto-react/client/components/MatchComp.jsx
@@ -4,12 +4,45 @@ import Match from '../src/match.js'
 export default class MatchComp extends React.Component {
     render() {
         const id = this.props.match.id;
-        const message = this.props.match.message();
 
         return (
             <div>
-                <span>{message}</span>
-                <button onClick={() => this.props.deleteMatch(id)}>x</button>
+                <div>
+                    <span>P1 Score</span>
+                    <input type="number" />
+                </div>
+                <div>
+                    <span>P1 Character</span>
+                    <select defaultValue="1" onChange={event => console.log(event.target.value)}>
+                        <option value="0">Mario</option>
+                        <option value="1">Link</option>
+                        <option>Sonic</option>
+                    </select>
+                </div>
+
+                <div>
+                    <span>P2 Score</span>
+                    <input type="number" />
+                </div>
+                <div>
+                    <span>P2 Character</span>
+                    <select>
+                        <option>Mario</option>
+                        <option>Link</option>
+                        <option>Sonic</option>
+                    </select>
+                </div>
+
+                <div>
+                    <span>Stage</span>
+                    <select>
+                        <option>Final Destination</option>
+                        <option>Battlefield</option>
+                        <option>Smashville</option>
+                    </select>
+                </div>
+
+                <button onClick={() => this.props.deleteMatch(id)}>Delete Match</button>
             </div>
         );
     }
--- proto-react/client/src/match.js
+++ proto-react/client/src/match.js
@@ -1,9 +1,10 @@
 export default class Match {
     constructor(id) {
         this.id = id;
-    }
-
-    message() {
-        return "Match " + this.id;
+        this.p1Score = 0;
+        this.p2Score = 0;
+        this.p1Characters = [];
+        this.p2Characters = [];
+        this.stage = null;
     }
 }
