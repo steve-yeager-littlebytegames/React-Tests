d0ba5973dcb4e294f7c8f5c0892636117ffa8638
Steve Yeager <steve.yeager@littlebytegames.com>
2018-03-14 11:30:17 -0700

    Add basic style for match inputs

--- proto-react/client/components/MatchComp.jsx
+++ proto-react/client/components/MatchComp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Match from '../src/match.js'
+import '../css/match.css';
 
 export default class MatchComp extends React.Component {
     render() {
@@ -8,48 +9,45 @@ export default class MatchComp extends React.Component {
         const matchCharPrefix = namePrefix + "matchCharacters[";
 
         return (
-            <div>
-                <div>
-                    <label>
-                        P1 Score
+            <div className="match">
+                <h4>Match {index + 1}</h4>
+                <input type="hidden" name={matchCharPrefix + "0].leagueUserID"} value="26" />
+                <input type="hidden" name={matchCharPrefix + "1].leagueUserID"} value="1" />
+
+                <label className="match-input">
+                    P1 Score
                     <input type="number" name={namePrefix + "player1score"} />
-                    </label>
-                </div>
-                <div>
-                    <input type="hidden" name={matchCharPrefix + "0].leagueUserID"} value="26" />
-                    <span>P1 Character</span>
-                    <select name={matchCharPrefix + "0].characterID"}>
+                </label>
+                <label className="match-input">
+                    P1 Character
+                        <select name={matchCharPrefix + "0].characterID"}>
                         <option value="72">Mario</option>
                         <option value="73">Link</option>
                         <option value="93">Sonic</option>
                     </select>
-                </div>
-
-                <div>
-                    <span>P2 Score</span>
+                </label>
+                <div className="input-break" />
+                <label className="match-input">
+                    P2 Score
                     <input type="number" name={namePrefix + "player2score"} />
-                </div>
-                <div>
-                    <input type="hidden" name={matchCharPrefix + "1].leagueUserID"} value="1" />
-                    <span>P2 Character</span>
-                    <select name={matchCharPrefix + "1].characterID"}>
+                </label>
+                <label className="match-input">
+                    P2 Character
+                        <select name={matchCharPrefix + "1].characterID"}>
                         <option value="72">Mario</option>
                         <option value="73">Link</option>
                         <option value="93">Sonic</option>
                     </select>
-                </div>
-
-                <div>
-                    <label>
-                        Stage
-                        <select name={namePrefix + "stageID"} >
-                            <option value="1">Final Destination</option>
-                            <option value="6">Battlefield</option>
-                            <option value="2">Smashville</option>
-                        </select>
-                    </label>
-                </div>
-
+                </label>
+                <div className="input-break" />
+                <label className="match-input">
+                    Stage
+                    <select name={namePrefix + "stageID"} >
+                        <option value="1">Final Destination</option>
+                        <option value="6">Battlefield</option>
+                        <option value="2">Smashville</option>
+                    </select>
+                </label>
                 <button onClick={() => this.props.deleteMatch(index)}>Delete Match</button>
             </div >
         );
